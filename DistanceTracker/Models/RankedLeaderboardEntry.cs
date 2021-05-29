641aaf76ae306e2a1a7e2460ea73cc518110ff2c
Scott Girton <girtonman@users.noreply.github.com>
2021-05-29 00:44:00 -0700

    Update appearance of the level page

--- DistanceTracker/Models/RankedLeaderboardEntry.cs
+++ DistanceTracker/Models/RankedLeaderboardEntry.cs
@@ -5,6 +5,8 @@
 		public int Rank { get; set; }
 		public double NoodlePoints { get; set; }
 		public double PlayerRating { get; set; }
+		public ulong Milliseconds { get; set; }
+		public ulong FirstSeenTimeUTC { get; set; }
 		public Player Player { get; set; }
 		public Leaderboard Leaderboard { get; set; }
 	}
