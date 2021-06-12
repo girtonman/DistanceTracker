2c337c9647c28685cff363ba0b923d5c807ef621
Scott Girton <girtonman@users.noreply.github.com>
2021-06-11 19:47:35 -0700

    Add real ordinal formatting
    Gets rid of the placeholder "th" and replaces with a function that makes a proper ordinal.

--- DistanceTracker/Utils/Formatter.cs
+++ DistanceTracker/Utils/Formatter.cs
@@ -49,5 +49,28 @@ namespace DistanceTracker
 				return seconds > 1 ? $"{seconds} seconds ago" : $"{seconds} second ago";
 			}
 		}
+
+		public static string GetOrdinal(int num)
+		{
+			switch (num % 100)
+			{
+				case 11:
+				case 12:
+				case 13:
+					return num + "th";
+			}
+
+			switch (num % 10)
+			{
+				case 1:
+					return num + "st";
+				case 2:
+					return num + "nd";
+				case 3:
+					return num + "rd";
+				default:
+					return num + "th";
+			}
+		}
 	}
 }
