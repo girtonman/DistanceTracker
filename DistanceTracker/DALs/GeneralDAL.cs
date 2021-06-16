90f41bd783bd514646c839c23e933469e61ca3d2
Scott Girton <girtonman@users.noreply.github.com>
2021-06-16 00:26:50 -0700

    Add site stats to the home page

---
+++ DistanceTracker/DALs/GeneralDAL.cs
@@ -0,0 +1,47 @@
+﻿using DistanceTracker.Models;
+using MySqlConnector;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DistanceTracker.DALs
+{
+	public class GeneralDAL
+	{
+		private MySqlConnection Connection { get; set; }
+
+		public GeneralDAL()
+		{
+			Connection = new MySqlConnection(Settings.ConnectionString);
+		}
+
+		public async Task<SiteStats> GetSiteStats()
+		{
+			Connection.Open();
+
+			var sql = @"
+			SELECT
+				(SELECT COUNT(*) FROM Players) PlayerCount,
+				(SELECT COUNT(*) FROM LeaderboardEntries) LeaderboardEntryCount,
+				(SELECT COUNT(*) FROM LeaderboardEntryHistory) ImprovementCount,
+				(SELECT COUNT(*) FROM Leaderboards) LeaderboardCount
+			FROM (SELECT 1) dummy
+			";
+
+			var command = new MySqlCommand(sql, Connection);
+			var reader = await command.ExecuteReaderAsync();
+
+			var siteStats = new SiteStats();
+			while(reader.Read())
+			{
+				siteStats.PlayerCount = reader.GetInt32(0);
+				siteStats.LeaderboardEntryCount = reader.GetInt32(1);
+				siteStats.ImprovementCount = reader.GetInt32(2);
+				siteStats.LeaderboardCount = reader.GetInt32(3);
+			}
+			reader.Close();
+			Connection.Close();
+
+			return siteStats;
+		}
+	}
+}
---
+++ DistanceTracker/Models/SiteStats.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DistanceTracker.Models
+{
+	public class SiteStats
+	{
+		public int PlayerCount { get; set; }
+		public int LeaderboardEntryCount { get; set; }
+		public int ImprovementCount { get; set; }
+		public int LeaderboardCount { get; set; }
+	}
+}
