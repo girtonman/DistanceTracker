b0a4a14a975e211b0028faf4608a44dae4d86e3b
Scott Girton <girtonman@users.noreply.github.com>
2022-02-23 19:21:37 -0800

    Add world record activity page

--- DistanceTracker/Controllers/HomeController.cs
+++ DistanceTracker/Controllers/HomeController.cs
@@ -60,6 +60,43 @@ namespace DistanceTracker.Controllers
 			return new JsonResult(recentActivity);
 		}
 
+		public IActionResult WRActivity() => View();
+
+		public async Task<IActionResult> GetWRActivity()
+		{
+			// Get data
+			var lehDAL = new LeaderboardEntryHistoryDAL();
+			var recentWRs = await lehDAL.GetRecentImprovements(100, rankCutoff: 1);
+
+			// Prepare empty view model
+			var recentActivity = new List<Activity>();
+
+			// Add data to view model
+			recentWRs.ForEach(x => recentActivity.Add(new Activity()
+			{
+				TimeUTC = x.UpdatedTimeUTC,
+				Improvement = x,
+			}));
+
+			// Ordering
+			recentActivity = recentActivity.OrderByDescending(x => x.TimeUTC).ToList();
+
+			// Add avatars
+			foreach (var activity in recentActivity)
+			{
+				if (activity.Sighting != null)
+				{
+					await activity.Sighting.Player.GetSteamAvatar();
+				}
+				else if (activity.Improvement != null)
+				{
+					await activity.Improvement.Player.GetSteamAvatar();
+				}
+			}
+
+			return new JsonResult(recentActivity);
+		}
+
 		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 		public IActionResult Error()
 		{
