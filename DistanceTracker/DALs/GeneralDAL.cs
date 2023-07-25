6894e9023b82cfa3d4edd8fb309c14fe51e1f15a
girtonman <girtonman@gmail.com>
2023-07-25 18:43:19 -0500

    Add notification on home page when scraper is probably dead

--- DistanceTracker/DALs/GeneralDAL.cs
+++ DistanceTracker/DALs/GeneralDAL.cs
@@ -22,12 +22,15 @@ namespace DistanceTracker.DALs
 				(SELECT COUNT(*) FROM Players) PlayerCount,
 				(SELECT COUNT(*) FROM LeaderboardEntries) LeaderboardEntryCount,
 				(SELECT COUNT(*) FROM LeaderboardEntryHistory) ImprovementCount,
-				(SELECT COUNT(*) FROM Leaderboards) LeaderboardCount
+				(SELECT COUNT(*) FROM Leaderboards) LeaderboardCount,
+				(SELECT MAX(UpdatedTimeUTC) FROM LeaderboardEntries) UpdatedEntryTimeUTC,
+				(SELECT MAX(UpdatedTimeUTC) FROM LeaderboardEntryHistory) UpdatedImprovementTimeUTC
 			FROM (SELECT 1) dummy
 			";
 
 			var command = new MySqlCommand(sql, Connection);
 			var reader = await command.ExecuteReaderAsync();
+
 
 			var siteStats = new SiteStats();
 			while(reader.Read())
@@ -36,6 +39,12 @@ namespace DistanceTracker.DALs
 				siteStats.LeaderboardEntryCount = reader.GetInt32(1);
 				siteStats.ImprovementCount = reader.GetInt32(2);
 				siteStats.LeaderboardCount = reader.GetInt32(3);
+
+				
+				var updatedEntryTime = reader.GetUInt64(4);
+				var updatedImprovementTime = reader.GetUInt64(5);
+				var updatedTime = updatedEntryTime > updatedImprovementTime ? updatedEntryTime : updatedImprovementTime;
+				siteStats.UpdatedTimeUTC = updatedTime;
 			}
 			reader.Close();
 			Connection.Close();
